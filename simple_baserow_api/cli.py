9b03742bc41cf71267903308c4b32d9ea543379f
Küchler, Oliver <oliver.kuechler@bih-charite.de>
2024-12-17 12:50:02 +0100

    Remove __init__.py to satisfy CLI

--- simple_baserow_api/cli.py
+++ simple_baserow_api/cli.py
@@ -1,11 +1,4 @@
 """CLI interface for simple_baserow_api project.
-
-Be creative! do whatever you want!
-
-- Install click or typer and create a CLI app
-- Use builtin argparse
-- Start a web application
-- Import things from your .base module
 """
 
 
